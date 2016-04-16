0586c285b35b70bfc0cabd13ca99c1a614d53fc2
Steven Jay <stevenjasionowicz@gmail.com>
2016-04-16 12:58:09 +1200

    Make local links component

--- src/pages/brands.js
+++ src/pages/brands.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import NavHelper from '../components/nav-helper'
 
 export default React.createClass({
 	render () {
 		return (
-			<div>
+			<NavHelper>
 				<h2>Brands</h2>
-			</div>	
+			</NavHelper>
 		)
 	}
 })
