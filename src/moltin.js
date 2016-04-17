52d4e5292dfc5fd015d8b820f76581f12b72e379
Steven Jay <stevenjasionowicz@gmail.com>
2016-04-17 20:41:26 +1200

    Add Moltin

---
+++ src/moltin.js
@@ -0,0 +1,8 @@
+<script src="https://js.moltin.com/v1"></script>
+
+<script type="text/javascript">
+  var moltin = new Moltin({publicId: 'api_client_id'});
+  moltin.Authenticate(function() {
+    // Code here...
+  });
+</script>
