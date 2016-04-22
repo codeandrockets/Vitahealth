7b22707ed220aeaffe55802692226cf11edb01cc
Steven Jay <stevenjasionowicz@gmail.com>
2016-04-23 11:22:47 +1200

    Add Order component into seperate file

--- scripts/main.js
+++ scripts/main.js
@@ -19,8 +19,9 @@ var Catalyst = require('react-catalyst');
 
 //**************************** IMPORT COMPONENTS *************************************//
 
-import Product from './components/Product';
 import Header from './components/Header';
+import Product from './components/Product';
+import Order from './components/Order';
 import NotFound from './components/NotFund';
 
 //App *********************  FRONT PAGE  *********************************************//
@@ -131,53 +132,6 @@ var AddProductForm = React.createClass({
 				<button type="submit">+ Add Item </button>
 			</form>	
 		)
-	}
-});
-
-//Order
-//<Order />
-var Order = React.createClass({
-	renderOrder : function(key) {
-		var product = this.props.products[key];
-		var count = this.props.order[key];
-		var removeButton = <button onClick={this.props.removeFromOrder.bind(null,key)}>&times;</button>
-
-		if(!product) {
-			return <li key={key}>Sorry, product no longer available! {removeButton}</li>
-		}
-
-		return (<li key={key}>
-					{count}x
-					{product.name}
-					<span className="price">{h.formatPrice(count * product.price)}</span>{removeButton}
-				</li>)
-	},
-	render : function() {
-		var orderIds = Object.keys(this.props.order);
-		var total = orderIds.reduce((prevTotal, key)=> {
-		var product = this.props.products[key];
-		var count = this.props.order[key];
-		var isAvailable = product && product.status === 'available';
-
-		if(product && isAvailable) {
-			return prevTotal + (count * parseInt(product.price) || 0 );
-		}
-
-		return prevTotal;
-
-		}, 0);
-	return (
-		<div className="order-wrap">
-			<h2 className="order-title">Your Order</h2>
-			<ul className="order">
-				{orderIds.map(this.renderOrder)}
-				<li className="total">
-					<strong>Total:</strong>
-					{h.formatPrice(total)}
-				</li>
-			</ul>
-		</div>
-		)	
 	}
 });
 
